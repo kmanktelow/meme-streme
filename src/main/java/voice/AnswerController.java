b80abd591d399cc4b49b5124cb511d7e806af96a
alexandraAp <alexandra@nexmo.com>
2019-06-23 22:23:07 +0300

    cleanup

--- src/main/java/voice/AnswerController.java
+++ src/main/java/voice/AnswerController.java
@@ -3,16 +3,14 @@ package voice;
 import com.nexmo.client.incoming.RecordEvent;
 import com.nexmo.client.voice.ncco.Action;
 import com.nexmo.client.voice.ncco.Ncco;
-import com.nexmo.client.voice.ncco.RecordAction;
 import com.nexmo.client.voice.ncco.TalkAction;
-import org.apache.commons.lang3.StringUtils;
 import spark.Route;
 import spark.Spark;
 
 public class AnswerController {
 
-    private static final String JWT = "";
-    private static String conversation_id = "";
+    private static final String THIS_APP_HOST = "ebd3525c.ngrok.io";
+    private static final String GO_APP_HOST = "3772bf9d.ngrok.io";
 
     public static void main(String[] args) {
         /*
@@ -20,32 +18,16 @@ public class AnswerController {
          */
         Route answerRoute = (req, res) -> {
             String[] params = req.queryString().split("&");
-            String user_number = params[0].split("=")[1];
-            String uuid = params[3].split("=")[1];
-            if (StringUtils.isBlank(conversation_id)) {
-                conversation_id = params[2].split("=")[1];
-            }
-            System.out.println("I am an uuid: " + uuid);
-            System.out.println("conversation id: " + conversation_id);
+            String user_number = params[1].split("=")[1];
 
             TalkAction intro = TalkAction.builder(
-                    "Please leave a message after the tone, then press #. We will get back to you as soon as we can.").build();
-
-            //TODO: create user too
-
-            String conversationName = "";//TODO: get conversation
-
-            //ConversationAction conversation = new ConversationAction.Builder(conversationName).build();
-
+                    "Hello, you have joined the call").build();
 
             Action websocketAction = getWebSocketAction(user_number);
 
-            TalkAction outro = TalkAction.builder("Thank you for your message. Goodbye").build();
-
             res.type("application/json");
 
-            String ncco = new Ncco(intro, websocketAction, outro).toJson();
-
+            String ncco = new Ncco(intro, websocketAction).toJson();
             System.out.println("I am a ncco " + ncco);
             return ncco;
         };
@@ -64,7 +46,6 @@ public class AnswerController {
 
         Route eventRoute = (req, res) -> {
             System.out.println("I am an event: " + req.body());
-            System.out.println("I am an event query string: " + req.queryString());
             res.status(200);
             return "";
         };
@@ -81,13 +62,13 @@ public class AnswerController {
     }
 
     private static Action getWebSocketAction(String user_number) {
-        String callback = "https://ebd3525c.ngrok.io/asr";
+        String callback = "https://" + THIS_APP_HOST +"/asr";
         WebSocketHeader header = new WebSocketHeader(user_number, callback);
 
         WebSocketEndpoint endpoint = new WebSocketEndpoint();
         endpoint.setType("websocket");
         endpoint.setContent_type("audio/l16;rate=16000");
-        endpoint.setUri("ws://3772bf9d.ngrok.io/echo");
+        endpoint.setUri("ws://"+ GO_APP_HOST + "/echo");
         endpoint.setHeaders(header);
 
         WebSocketAction action = new WebSocketAction();
