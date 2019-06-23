9159140152c47c3ef0992002aa1465218d083137
kmanktelow <karen.manktelow@vonage.com>
2019-06-23 09:47:48 +0100

    gitignore, pip file

---
+++ meme_streme.py
@@ -0,0 +1,9 @@
+import nexmo
+
+client = nexmo.Client(key="", secret="")
+
+response = client.create_call({
+  'to': [{'type': 'phone', 'number': '14843331234'}],
+  'from': {'type': 'phone', 'number': '14843335555'},
+  'answer_url': ['https://example.com/answer']
+})
